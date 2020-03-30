535220e2173dafe926f18c007251b2fbaf7bb51a
kimsunin <tjsdls0724@naver.com>
2020-03-30 14:48:50 +0900

    test

---
+++ mi/js/join.js
@@ -0,0 +1,9 @@
+//모달창 
+function chkDetail1(){
+    $("#agreeModal1").show();
+}
+
+function allchkContentclose(){
+    $('#agreeModal1').css("display",'none');
+}
+
---
+++ mi/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.cardContainer').cardcontainer({
+        auto:true,
+        speed:500,
+        pause:5000,
+        mode:'horizontal',
+        autoControls:true,
+        pager:true,
+        captions:true,
+    });
+})
